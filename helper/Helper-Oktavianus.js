3600fc85c1d6678a0b4e4fe057d49e475b0c44e9
OktavianusHendry <oktavianus.hendry@student.pradita.ac.id>
2023-03-24 16:31:03 +0700

    second

---
+++ helper/Helper-Oktavianus.js
@@ -0,0 +1,24 @@
+class Hitungan {
+    static pembagian(a, b) {
+        return a / b;
+    }
+
+    static pengurangan(a, b) {
+        return a - b;
+    }
+
+    static perkalian(a, b) {
+        return a * b;
+    }
+
+    static penjumlahan(a, b) {
+        return a + b;
+    }
+}
+
+module.exports = Hitungan;
+
+// console.log(Hitungan.pembagian(10, 2)); // Output: 5
+// console.log(Hitungan.pengurangan(10, 2)); // Output: 8
+// console.log(Hitungan.perkalian(10, 2)); // Output: 20
+// console.log(Hitungan.penjumlahan(10, 2)); // Output: 12
---
+++ index.js
@@ -0,0 +1,6 @@
+const Hitungan = require("./helper/Helper-Oktavianus");
+
+console.log(Hitungan.pembagian(10, 2)); // Output: 5
+console.log(Hitungan.pengurangan(10, 2)); // Output: 8
+console.log(Hitungan.perkalian(10, 2)); // Output: 20
+console.log(Hitungan.penjumlahan(10, 2)); // Output: 12
